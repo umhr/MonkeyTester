2fce0e0d4a738c041e166c4e44a678e0ea928b3e
Hiroshi Umehara <com@umehara.net>
2013-11-16 22:20:29 +0900

    add

---
+++ MonkeyTester/MonkeyTester/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace MonkeyTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2_interval = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2_interval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(1000, 23);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(1, 29);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1000, 600);
+            this.webBrowser1.TabIndex = 7;
+            this.webBrowser1.Url = new System.Uri("http://yahoo.co.jp", System.UriKind.Absolute);
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "操作回数";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(13, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numericUpDown2_interval);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Location = new System.Drawing.Point(1001, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(120, 629);
+            this.panel1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Interval";
+            // 
+            // numericUpDown2_interval
+            // 
+            this.numericUpDown2_interval.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown2_interval.Location = new System.Drawing.Point(37, 197);
+            this.numericUpDown2_interval.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2_interval.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown2_interval.Name = "numericUpDown2_interval";
+            this.numericUpDown2_interval.Size = new System.Drawing.Size(80, 22);
+            this.numericUpDown2_interval.TabIndex = 6;
+            this.numericUpDown2_interval.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0 / 100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(37, 135);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 629);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MonkeyTester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2_interval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2_interval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ MonkeyTester/MonkeyTester/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MonkeyTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // http://dobon.net/vb/dotnet/system/cursorposition.html
+            //        System.Windows.Forms.Cursor.Position = this.PointToScreen(new System.Drawing.Point(0, 0));
+        }
+        // http://homepage2.nifty.com/nonnon/SoftSample/CS.NET/SampleSendInput.html
+        // マウスイベント(mouse_eventの引数と同様のデータ)
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
+        };
+
+        // キーボードイベント(keybd_eventの引数と同様のデータ)
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KEYBDINPUT
+        {
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
+        };
+
+        // ハードウェアイベント
+        [StructLayout(LayoutKind.Sequential)]
+        private struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        };
+
+        // 各種イベント(SendInputの引数データ)
+        [StructLayout(LayoutKind.Explicit)]
+        private struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        };
+
+        // キー操作、マウス操作をシミュレート(擬似的に操作する)
+        [DllImport("user32.dll")]
+        private extern static void SendInput(
+            int nInputs, ref INPUT pInputs, int cbsize);
+
+        // 仮想キーコードをスキャンコードに変換
+        [DllImport("user32.dll", EntryPoint = "MapVirtualKeyA")]
+        private extern static int MapVirtualKey(
+            int wCode, int wMapType);
+
+        private const int INPUT_MOUSE = 0;                  // マウスイベント
+        private const int INPUT_KEYBOARD = 1;               // キーボードイベント
+        private const int INPUT_HARDWARE = 2;               // ハードウェアイベント
+
+        private const int MOUSEEVENTF_MOVE = 0x1;           // マウスを移動する
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;    // 絶対座標指定
+        private const int MOUSEEVENTF_LEFTDOWN = 0x2;       // 左　ボタンを押す
+        private const int MOUSEEVENTF_LEFTUP = 0x4;         // 左　ボタンを離す
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x8;      // 右　ボタンを押す
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;       // 右　ボタンを離す
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x20;    // 中央ボタンを押す
+        private const int MOUSEEVENTF_MIDDLEUP = 0x40;      // 中央ボタンを離す
+        private const int MOUSEEVENTF_WHEEL = 0x800;        // ホイールを回転する
+        private const int WHEEL_DELTA = 120;                // ホイール回転値
+
+        private const int KEYEVENTF_KEYDOWN = 0x0;          // キーを押す
+        private const int KEYEVENTF_KEYUP = 0x2;            // キーを離す
+        private const int KEYEVENTF_EXTENDEDKEY = 0x1;      // 拡張コード
+        private const int VK_SHIFT = 0x10;                  // SHIFTキー
+        private int _count = 0;
+        private Timer _timer = new Timer();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _count = 0;
+            label1.Text = "" + _count + " / " + numericUpDown1.Value;
+
+            _timer.Interval = Decimal.ToInt16(numericUpDown2_interval.Value);
+            _timer.Tick += tick;
+            _timer.Start();
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+            if (_count >= numericUpDown1.Value)
+            {
+                _timer.Stop();
+                System.Media.SystemSounds.Beep.Play();
+                return;
+            }
+
+            Random random = new Random();
+            int ran = random.Next(20);
+            if (ran < 3)
+            {
+                mouseWheel();
+            }
+            else
+            {
+                mouseRandom();
+            }
+
+            _count++;
+
+            label1.Text = "" + _count + " / " + numericUpDown1.Value;
+        }
+
+        private void mouseRandom()
+        {
+            // マウス操作実行用のデータ
+            const int num = 3;
+            INPUT[] inp = new INPUT[num];
+            inp = randomMove(inp);
+            // マウス操作実行
+            SendInput(num, ref inp[0], Marshal.SizeOf(inp[0]));
+        }
+
+        private INPUT[] randomMove(INPUT[] inp)
+        {
+
+            // (1)マウスカーソルを移動する(スクリーン座標でX座標=800ピクセル,Y=400ピクセルの位置)
+            inp[0].type = INPUT_MOUSE;
+            inp[0].mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+            Point p0 = new Point();
+            Random random = new Random();
+            p0.X = webBrowser1.Location.X + random.Next(webBrowser1.Size.Width);
+            p0.Y = webBrowser1.Location.Y + random.Next(webBrowser1.Size.Height);
+            Point p1 = this.PointToScreen(p0);
+
+            inp[0].mi.dx = p1.X * (65535 / Screen.PrimaryScreen.Bounds.Width);
+            inp[0].mi.dy = p1.Y * (65535 / Screen.PrimaryScreen.Bounds.Height);
+            inp[0].mi.mouseData = 0;
+            inp[0].mi.dwExtraInfo = 0;
+            inp[0].mi.time = 0;
+
+            // (2)マウスの右ボタンを押す
+            inp[1].type = INPUT_MOUSE;
+            inp[1].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            inp[1].mi.dx = 0;
+            inp[1].mi.dy = 0;
+            inp[1].mi.mouseData = 0;
+            inp[1].mi.dwExtraInfo = 0;
+            inp[1].mi.time = 0;
+
+            // (3)マウスの右ボタンを離す
+            inp[2].type = INPUT_MOUSE;
+            inp[2].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            inp[2].mi.dx = 0;
+            inp[2].mi.dy = 0;
+            inp[2].mi.mouseData = 0;
+            inp[2].mi.dwExtraInfo = 0;
+            inp[2].mi.time = 0;
+            return inp;
+        }
+
+        // マウスホイールを回転する
+        private void mouseWheel()
+        {
+            // マウス操作実行用のデータ
+            const int num = 1;
+            INPUT[] inp = new INPUT[num];
+
+            // (2)マウスホイールを前方(近づく方向)へ回転する
+            inp[0].type = INPUT_MOUSE;
+            inp[0].mi.dwFlags = MOUSEEVENTF_WHEEL;
+            inp[0].mi.dx = 0;
+            inp[0].mi.dy = 0;
+            inp[0].mi.mouseData = (new Random().Next(10) - 6) * WHEEL_DELTA;
+            inp[0].mi.dwExtraInfo = 0;
+            inp[0].mi.time = 0;
+
+            // マウス操作実行
+            SendInput(num, ref inp[0], Marshal.SizeOf(inp[0]));
+            System.Threading.Thread.Sleep(10);
+        }
+
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    string url = comboBox1.Text;
+                    if (!comboBox1.Text.StartsWith("http://")) url = "http://" + url;
+                    webBrowser1.Navigate(new Uri(url));
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private String currentURL;
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            // コンボボックスのテキスト変更 
+            this.Text = webBrowser1.Document.Title.ToString();
+            this.comboBox1.Text = webBrowser1.Url.ToString();
+
+            if (currentURL != comboBox1.Text)
+            {
+                comboBox1.Items.Add(comboBox1.Text);
+                currentURL = comboBox1.Text;
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                webBrowser1.Navigate(comboBox1.Text);
+            }
+            catch
+            {
+            }
+        }
+
+
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Point p = panel1.Location;
+            p.X = this.Width - 120;
+            panel1.Location = p;
+
+            webBrowser1.Width = this.Width - 120;
+            webBrowser1.Height = this.Height - webBrowser1.Location.Y - 38;
+            comboBox1.Width = webBrowser1.Width - comboBox1.Location.Y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1_Resize(null, null);
+        }
+
+
+    }
+}
